8b869891596066b9c1f56a12efdfbe50a542d5dc
GURU PRASATH C <guru630172@gmail.com>
2026-06-15 13:23:40 +0530

    Time: 0 ms (100%), Space: 62.8 MB (78.01%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    int maxArea(vector<int>&he){
-        int n=he.size();
+    int maxArea(vector<int>&height){
+        //two pointers
+        int n=height.size();
         int l=0;
-        int r=he.size()-1;
+        int r=height.size()-1;
         int area=0;
         int marea=INT_MIN;
         while(l<r){
-            int h=min(he[l],he[r]);
+            int h=min(height[l],height[r]);
             int w=r-l;
             area=w*h;
             marea=max(marea,area);
-            if(he[l]<=he[r]) l++;
+            if(height[l]<=height[r]) l++;
             else r--;
         }
         return marea;
