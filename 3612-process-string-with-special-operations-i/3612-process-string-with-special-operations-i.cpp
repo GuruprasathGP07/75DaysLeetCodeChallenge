c48ba436e58368ff53e386c3266f65f83bc5256a
GURU PRASATH C <guru630172@gmail.com>
2026-06-16 09:57:18 +0530

    Time: 3 ms (76.89%), Space: 39.9 MB (42.05%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -1,50 +1,13 @@
 class Solution {
 public:
     string processStr(string s) {
-        /*stack<char>st;
-        int n=s.length();
-        for(int i=0;i<n;i++){
-            if(isalpha(s[i])) st.push(s[i]);
-            else if(s[i]=='*' && !st.empty()) st.pop();
-            else if(s[i]=='#' && !st.empty()){
-                stack<char>temp=st;
-                string str="";
-                while(!temp.empty()){
-                    str+=temp.top();
-                    temp.pop();
-                }
-                reverse(str.begin(),str.end());
-                for(char c:str){
-                    st.push(c);
-                }
-
-
-            }
-            else{
-                stack<char>s;
-                while(!st.empty()){
-                    s.push(st.top());
-                    st.pop();
-                }
-                swap(s,st);
-            }
-        }
-        string ans="";
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-        */
         string st="";
         int n=s.length();
         for(int i=0;i<n;i++){
             if(isalpha(s[i])) st+=s[i];
             else if(s[i]=='*' && !st.empty()) st.pop_back();
             else if(s[i]=='#' && !st.empty()){
-               string t=st;
-               st+=t;
+               st+=st;
             }
             else{
                 reverse(st.begin(),st.end());
