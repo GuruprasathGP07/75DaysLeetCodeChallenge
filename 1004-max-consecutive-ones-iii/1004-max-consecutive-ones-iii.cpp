51a952344972cf7f715502786bd04b4283f7a0c7
GURU PRASATH C <guru630172@gmail.com>
2026-06-17 11:20:08 +0530

    Time: 3 ms (34.28%), Space: 69.5 MB (92.52%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,15 +4,15 @@ public:
         int l=0,r=0;
         int c=0,z=0,ans=INT_MIN;
         while(r<nums.size()){
-            if(nums[r]==0) z++;
-            else c++;
+            if(nums[r]==1) c++;
+            else z++;
             int t=c+z;
             while(z>k){
                 if(nums[l]==1) c--;
                 else z--;
                 l++;
             }
-            ans=max(ans,(r-l+1));
+            ans=max(ans,r-l+1);
             r++;
         }
         return ans;
