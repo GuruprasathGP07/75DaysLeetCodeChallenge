326d321dd3fdbcc98000783558859817a441c1d5
GURU PRASATH C <guru630172@gmail.com>
2026-06-17 14:24:27 +0530

    Time: 0 ms (100%), Space: 10.2 MB (24.38%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.cpp
@@ -3,15 +3,16 @@ public:
     int equalSubstring(string s, string t, int mc) {
         int l=0,r=0;
         int ans=0;
+        /*
         vector<int>cost;
         for(int i=0;i<s.length();i++){
             cost.push_back(abs(s[i]-t[i]));
-        }
+        }*/
         int sum=0;
-        while(r<cost.size()){
-            sum+=cost[r];
+        while(r<s.length()){
+            sum+=abs(s[r]-t[r]);
             while(sum>mc){
-                sum-=cost[l];
+                sum-=abs(s[l]-t[l]);
                 l++;
             }
             ans=max(ans,(r-l+1));
