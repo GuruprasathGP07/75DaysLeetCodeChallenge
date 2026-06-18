3da2f57cd62b054976e01ebe9c3e286e39387e9a
GURU PRASATH C <guru630172@gmail.com>
2026-06-18 13:28:02 +0530

    Time: 0 ms (100%), Space: 37.2 MB (5.46%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        //prefix sum
+
+        vector<int>lsum(nums.size());
+        vector<int>rsum(nums.size());
+        lsum[0]=0;
+        rsum[nums.size()-1]=0;
+        for(int i=1;i<nums.size();i++){
+            lsum[i]+=lsum[i-1]+nums[i-1];
+
+        }
+        for(int i=nums.size()-2;i>=0;i--){
+            rsum[i]=rsum[i+1]+nums[i+1];
+        }
+        for(int i=0;i<nums.size();i++){
+            if(lsum[i]==rsum[i]) return i;
+
+        }
+        return -1;
+    }
+};
