2fe5ca3e08d1ad837efc15907dea6798c4c6f904
GURU PRASATH C <guru630172@gmail.com>
2026-06-19 13:14:11 +0530

    Time: 0 ms (100%), Space: 10.8 MB (73.87%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int ans=0;
-        int alt=0;
+        int s=0,ans=0;
         for(int i=0;i<gain.size();i++){
-            alt+=gain[i];
-            ans=max(ans,alt);
+            s+=gain[i];
+            ans=max(ans,s);
         }
         return ans;
+        
     }
 };
