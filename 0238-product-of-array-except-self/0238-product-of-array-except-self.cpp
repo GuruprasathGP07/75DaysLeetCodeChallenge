3c2ef187685ceb5ab33cd8256cb64243865aceed
GURU PRASATH C <guru630172@gmail.com>
2026-06-19 10:50:01 +0530

    Time: 2 ms (43.15%), Space: 40.3 MB (60.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-     int  z=0,pro=1;   
-     for(int i:nums){
-        if(i==0)z++;       
-        else pro*=i;
-     } 
-     for(int i=0;i<nums.size();i++){
-        if(z>1) nums[i]=0;
-        else if(z==1){
-            if(nums[i]==0) nums[i]=pro;
-            else nums[i]=0;
+        vector<int>lp(nums.size());
+        lp[0]=1;
+        for(int i=1;i<nums.size();i++){
+            lp[i]=lp[i-1]*nums[i-1];
         }
-        else{
-            nums[i]=pro/nums[i];
+        int rp=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            lp[i]=rp*lp[i];
+            rp*=nums[i];
         }
-     }
-     return nums;
+        return lp;
     }
 };
