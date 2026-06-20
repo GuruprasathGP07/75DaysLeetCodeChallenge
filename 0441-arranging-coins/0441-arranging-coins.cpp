5c706c82f67d0cd1defb4ce3570fe11bd559f161
GURU PRASATH C <guru630172@gmail.com>
2026-06-20 11:39:12 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.78%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ans=0;
+        /*int ans=0;
         int s=1;
         while(n>=s){
             n-=s;
@@ -9,6 +9,21 @@ public:
 
         }
         return s-1;
-        
+        */
+        int l=1,r=n;
+        int ans=0;
+        while(l<=r){
+            long int mid=l+(r-l)/2;
+            long int coins=(mid*(mid+1))/2;
+            if(coins<=n){
+                ans=mid;
+                l=mid+1;
+
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return ans;
     }
 };
