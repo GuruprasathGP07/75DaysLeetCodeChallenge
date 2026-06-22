017f770451c896f80826780d8b05840672566a64
GURU PRASATH C <guru630172@gmail.com>
2026-06-22 09:19:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (17.68%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(long int i=1;i*i<=num;i++){
+        /*for(long int i=1;i*i<=num;i++){
             if(i*i==num) return true;
+        }
+        return false;*/
+
+
+        //Binary Search 
+        long long int l=1,r=num;
+        while(l*l<=num){
+            long long int mid=(l+r)/2;
+            if(mid*mid==num) return true;
+            else if(mid*mid<num) l=mid+1;
+            else r=mid-1;
         }
         return false;
     }
