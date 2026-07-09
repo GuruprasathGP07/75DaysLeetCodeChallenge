b73359c14daaa3d338c9fc0f54d1476e476dcdf3
GURU PRASATH C <guru630172@gmail.com>
2026-07-09 05:31:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.52%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        /*return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);*/
+        int t0=0;
+        int t1=1,t2=1;
+        for(int i=3;i<=n;i++){
+            int t3=t0+t1+t2;
+            t0=t1;
+            t1=t2;
+            t2=t3;
+
+        }
+
+
+
+
+        return t2;
+        
+    }
+};
