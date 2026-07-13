72c426e1c7f0cc584b1e1c4da50210856c2ca71b
GURU PRASATH C <guru630172@gmail.com>
2026-07-13 14:26:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
+    //recursion 
+    /*int climb(int n,vector<int>&dp){
+        if(n<=2) return n;
+        if(dp[n]!=-1) return dp[n];
+        else return dp[n]=climb(n-1,dp)+climb(n-2,dp);
+    }*/
     int climbStairs(int n) {
-        int arr[n+1];
-        if(n==1) return 1;
-        arr[1]=1;
-        arr[2]=2;
+        /*if(n<=2) return n;
+        return climbStairs(n-1)+climbStairs(n-2);*/
+        //tabuation 
+        if(n==1 || n==2) return n;
+        vector<int>dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
         for(int i=3;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return arr[n];
+        return dp[n];
     }
 };
