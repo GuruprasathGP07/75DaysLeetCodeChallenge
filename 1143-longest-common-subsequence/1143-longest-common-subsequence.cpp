599995fb91fc240ba45ef947c39281955011987b
GURU PRASATH C <guru630172@gmail.com>
2026-07-14 15:59:17 +0530

    Time: 24 ms (71.38%), Space: 27.5 MB (30.9%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    int find(int n1,int n2,string& t1,string& t2,vector<vector<int>>&dp){
-        if(n1<0 || n2<0) return 0;
-        if(dp[n1][n2]!=-1) return dp[n1][n2];
-        if(t1[n1]==t2[n2]) return dp[n1][n2]=1+find(n1-1,n2-1,t1,t2,dp);
-        int l=find(n1,n2-1,t1,t2,dp);
-        int r=find(n1-1,n2,t1,t2,dp);
-        return dp[n1][n2]=max(l,r);
+    int LCS(int n1,int n2,string &t1,string &t2,vector<vector<int>>&dp){
+        if(n1<0 || n2<0 ) return 0;
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(t1[i-1]==t2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else{
+                    int l=dp[i][j-1];
+                    int r=dp[i-1][j];
+                    dp[i][j]= max(l,r);
+                }
+            }
+        }
+        return dp[n1][n2];
     }
+    //tabulation method
     int longestCommonSubsequence(string t1,string t2) {
         int n1=t1.length();
         int n2=t2.length();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return find(n1-1,n2-1,t1,t2,dp);
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        return LCS(n1,n2,t1,t2,dp);
     }
 };
