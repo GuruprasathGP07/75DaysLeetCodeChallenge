2284e33d824fab65173c3e7dc5fb94eb291e4ab0
GURU PRASATH C <guru630172@gmail.com>
2026-09-09 23:17:32 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -11,11 +11,11 @@ public:
     long long countCommas(long long n) {
         int c=countdig(n);
         long long ans=0;
-        if(c>=16)ans+=n-999999999999999L;
-        if(c>=13) ans+=n-999999999999L;
-        if(c>=10)ans+=n-999999999L;
-        if(c>=7) ans+=n-999999L;
-        if(c>=4)ans+=n-999LL;
+        if(c>=16)ans+=n-999999999999999;
+        if(c>=13) ans+=n-999999999999;
+        if(c>=10)ans+=n-999999999;
+        if(c>=7) ans+=n-999999;
+        if(c>=4)ans+=n-999;
 
         return ans; 
 
